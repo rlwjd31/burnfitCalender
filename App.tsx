7c76d8aee206e83309a3e9faffd52093b8453160
rlwjd31 <rlwjd31@naver.com>
2025-01-21 22:30:26 +0900

    🚀 [Feat]: Bottom Navigation 간단 구현
    하드 코딩이로 bottom navigation에 필요한 코드를 작성함.
    아래는 route 정보들.
    
    - Home: HomePage navigate route
    - Calendar: CalendarPage navigate route
    - Library: LibraryPage navigate route
    - MyPage: MyPage navigate route

--- App.tsx
+++ App.tsx
@@ -6,21 +6,55 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
+const Tab = createBottomTabNavigator();
+
+function HomePage() {
+  return (
+    <View>
+      <Text>HomePage</Text>
+    </View>
+  );
+}
+
+function CalendarPage() {
+  return (
+    <View>
+      <Text>CalendarPage</Text>
+    </View>
+  );
+}
+
+function LibraryPage() {
+  return (
+    <View>
+      <Text>LibraryPage</Text>
+    </View>
+  );
+}
+
+function MyPage() {
+  return (
+    <View>
+      <Text>Message</Text>
+    </View>
+  );
+}
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>project settings...</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Calendar" component={CalendarPage} />
+        <Tab.Screen name="Library" component={LibraryPage} />
+        <Tab.Screen name="MyPage" component={MyPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
