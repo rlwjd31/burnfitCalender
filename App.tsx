0bb63db21fe42db33d83392f247c3b074d783165
rlwjd31 <rlwjd31@naver.com>
2025-01-22 06:09:16 +0900

    🚀 [Feat]: Navigation Icon 적용 & route 매핑
    각 navigation에 맞는 icon을 적용하고 route를 Tab.Screen의 name에 작성하여 해당 컴포넌트로 navigate되게 구현

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
@@ -7,6 +8,10 @@ import CalendarPage from './src/components/pages/Calendar';
 import LibraryPage from './src/components/pages/Library';
 import MyPage from './src/components/pages/MyPage';
 import {ROUTES} from './src/constants/route';
+import Home from './src/assets/icons/home.svg';
+import Calendar from './src/assets/icons/calendar.svg';
+import Barbell from './src/assets/icons/barbell.svg';
+import User from './src/assets/icons/user.svg';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,12 +21,46 @@ function App(): React.JSX.Element {
       <Tab.Navigator
         initialRouteName="Home"
         screenOptions={{
-          headerShown: false
+          headerShown: false,
+          tabBarActiveTintColor: '#3fc3e4',
+          tabBarInactiveTintColor: '#000',
         }}>
-        <Tab.Screen name={ROUTES.HOME} component={HomePage} />
-        <Tab.Screen name={ROUTES.CALENDER} component={CalendarPage} />
-        <Tab.Screen name={ROUTES.LIBRARY} component={LibraryPage} />
-        <Tab.Screen name={ROUTES.MyPage} component={MyPage} />
+        <Tab.Screen
+          name={ROUTES.HOME}
+          component={HomePage}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Home width={size} height={size} fill={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={ROUTES.CALENDER}
+          component={CalendarPage}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Calendar width={size} height={size} fill={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={ROUTES.LIBRARY}
+          component={LibraryPage}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Barbell width={size} height={size} fill={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={ROUTES.MyPage}
+          component={MyPage}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <User width={size} height={size} fill={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
