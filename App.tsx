6a17af2bd7201f4548c0bc3c756e40f330017c14
rlwjd31 <rlwjd31@naver.com>
2025-01-22 20:36:47 +0900

    ♻️ [Refactor]: BottomTopNavigation 추출
    다른 navigation이 추가되는 상황을 고려하여 NavigationContainer Wrapper는 App.tsx에 두고 bottomNavigation만 따로 추출하여 App에서는 전반적인 기능을 파악하기 쉽도록 하고 navigation에 관련된 것은 BottomTabNavigation내부로 위치시켜 응집도를 개선

--- App.tsx
+++ App.tsx
@@ -1,75 +1,12 @@
-/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomePage from './src/components/pages/Home';
-import CalendarPage from './src/components/pages/Calendar';
-import LibraryPage from './src/components/pages/Library';
-import MyPage from './src/components/pages/MyPage';
-import {ROUTES} from './src/constants/route';
-import Icon from './src/components/Icon';
-
-const Tab = createBottomTabNavigator();
+import BottomTabNavigation from './src/components/BottomTabNavigation';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: '#3fc3e4',
-          tabBarInactiveTintColor: '#000',
-        }}>
-        <Tab.Screen
-          name={ROUTES.HOME}
-          component={HomePage}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Icon iconType="home" width={size} height={size} fill={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name={ROUTES.CALENDER}
-          component={CalendarPage}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Icon
-                iconType="calendar"
-                width={size}
-                height={size}
-                fill="#fff"
-                stroke={color}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name={ROUTES.LIBRARY}
-          component={LibraryPage}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Icon
-                iconType="barbell"
-                width={size}
-                height={size}
-                fill={color}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name={ROUTES.MyPage}
-          component={MyPage}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Icon iconType="user" width={size} height={size} fill={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      <BottomTabNavigation />
     </NavigationContainer>
   );
 }
