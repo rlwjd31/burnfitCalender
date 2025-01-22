75fbb5b541208448d88971a23c03efb1f0fcb7e2
rlwjd31 <rlwjd31@naver.com>
2025-01-23 04:18:18 +0900

    🔨 [Chore]: Tailwind의 color를 extend하도록 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   theme: {
-    colors: {
-      red: '#B58888',
-      blue: '#3fc3e4',
-      blue1: '#4C789D',
-      gray: '#E7E7E7',
-      gray1: '#CBCFD2',
+    extend: {
+      colors: {
+        red: '#B58888',
+        blue: '#3fc3e4',
+        blue1: '#4C789D',
+        gray: '#E7E7E7',
+        gray1: '#CBCFD2',
+      },
     },
   },
 };
