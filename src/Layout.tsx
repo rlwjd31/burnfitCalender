0632687c7a50d31a5397e2b2673c9d9ac53d2861
rlwjd31 <rlwjd31@naver.com>
2025-01-23 04:31:30 +0900

    🚀 [Feat]: Calender UI WireFrame 설계
    달력 component의 WireFrame을 설계
    아래 두 가지로 이루어짐
    
    - calendar header
    - calendar body

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {ReactNode} from 'react';
-import {SafeAreaViewTW, ViewTW} from './components/common';
+import {SafeAreaViewTW} from './components/common';
 
 type LayoutProps = {
   children: ReactNode;
@@ -9,8 +9,8 @@ type LayoutProps = {
 
 export default function Layout({children}: LayoutProps) {
   return (
-    <SafeAreaViewTW className="flex-1 p-4">
-      <ViewTW>{children}</ViewTW>
+    <SafeAreaViewTW className="flex-1 p-4 bg-teal-200">
+      {children}
     </SafeAreaViewTW>
   );
 }
--- src/components/pages/Calendar.tsx
+++ src/components/pages/Calendar.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Layout from '../../Layout';
 import {TextTW, ViewTW} from '../common';
 import {now} from '../../constants/dayConstants';
+import {getAllDaysInMonth} from '../../libs/dayUtil';
+import {Dayjs} from 'dayjs';
 
 export default function CalendarPage() {
-  console.log('now =>', now);
+  const [nowState, setNowState] = useState<Dayjs>(now);
+  const allDaysInMonth = getAllDaysInMonth(nowState);
+  console.log('allDaysInMonth', allDaysInMonth);
 
   return (
     <Layout>
-      <ViewTW className="h-full items-center justify-center">
-        <TextTW>Calendar</TextTW>
+      <ViewTW className="bg-slate-200">
+        {/* calendar header */}
+        <ViewTW className="items-center justify-center">
+          <TextTW>Calendar Header</TextTW>
+        </ViewTW>
+        {/* calendar body */}
+        <ViewTW>
+          <ViewTW className="items-center bg-orange-200">
+            <TextTW>요일 section</TextTW>
+          </ViewTW>
+          <ViewTW className="items-center justify-center">
+            {allDaysInMonth.map(week => (
+              <ViewTW className="flex-row bg-red w-full">
+                <TextTW className="flex-1">day</TextTW>
+                <TextTW className="flex-1">day</TextTW>
+                <TextTW className="flex-1">day</TextTW>
+                <TextTW className="flex-1">day</TextTW>
+                <TextTW className="flex-1">day</TextTW>
+                <TextTW className="flex-1">day</TextTW>
+                <TextTW className="flex-1">day</TextTW>
+              </ViewTW>
+            ))}
+          </ViewTW>
+        </ViewTW>
       </ViewTW>
     </Layout>
   );
